8b0a9379f055c789fb3be4522e9f3973a86163c2
Nick Spreitzer <nick@refactorsaurusrex.com>
2015-10-18 13:48:30 -0500

    Add file launcher class

---
+++ ExcelLaunchPad/Rawr.Console/FileLauncher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rawr.LaunchPad.ConsoleApp
+{
+    public class FileLauncher
+    {
+        public FileLauncher(string filePath)
+        {
+            
+        }
+    }
+}
