15b8f113975e7327baa733efab9648d52bb33bee
Nick Spreitzer <nick@refactorsaurusrex.com>
2015-10-19 12:45:20 -0500

    Parse file path command line option

---
+++ ExcelLaunchPad/Rawr.Console/AppArguments.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rawr.LaunchPad.ConsoleApp
+{
+    public class AppArguments
+    {
+        public string FilePath { get; set; }
+    }
+}
