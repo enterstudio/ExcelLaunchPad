59b82d849a002c3036b9c2884a5e6c0794f3303f
Nick Spreitzer <nick@refactorsaurusrex.com>
2016-05-22 14:23:32 -0500

    Add custom installer project

---
+++ ExcelLaunchPad/Rawr.LaunchPad.Installer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rawr.LaunchPad.Installer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
