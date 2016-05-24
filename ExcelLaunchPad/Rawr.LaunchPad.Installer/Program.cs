8bc985c720876e2f92e52ce976cf15c176d16c95
Nick Spreitzer <nick@refactorsaurusrex.com>
2016-05-24 11:14:10 -0500

    Flesh out initial installer instructions

--- ExcelLaunchPad/Rawr.LaunchPad.Installer/Program.cs
+++ ExcelLaunchPad/Rawr.LaunchPad.Installer/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentConsole.Library;
 
 namespace Rawr.LaunchPad.Installer
 {
@@ -10,7 +13,68 @@ namespace Rawr.LaunchPad.Installer
     {
         static void Main(string[] args)
         {
+            var target = new Target();
+            var targetPath = target.FullPath();
+            if (targetPath == null)
+            {
+                "Whoops!".WriteLine(ConsoleColor.Red, 2);
 
+                ("Both ExcelLaunchPad.exe and ExcelLaunchPadInstaller.exe MUST be placed in the same directory. You can move the directory anywhere you want, " +
+                 "as long as both files are in the same folder.").WriteLine(2);
+
+                "Please move the files back into the same folder and try re-running the installer.".WriteLineWait();
+                return;
+            }
+
+            "***ExcelLaunchPad Installer***".WriteLine(ConsoleColor.Yellow, 2);
+            "(1) Install or Update".WriteLine();
+            "(2) Uninstall".WriteLine(1);
+            var key = "Please press 1 or 2 to continue...".WriteLineWait();
+
+            var validKeys = new List<ConsoleKey> { ConsoleKey.D1, ConsoleKey.D2, ConsoleKey.NumPad1, ConsoleKey.NumPad2 };
+            while (!validKeys.Contains(key.Key))
+                key = "That was not a valid response. Please enter 1 or 2.".WriteLineWait(ConsoleColor.Red);
+
+            switch (key.Key)
+            {
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+                    Install(targetPath);
+                    break;
+
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2:
+                    UnInstall();
+                    break;
+            }
+        }
+
+        static void Install(string targetPath)
+        {
+            var regEdit = new RegistryEditor();
+            try
+            {
+                regEdit.AddEntries(targetPath);
+                "Done! You should now be able to right-click any Excel-compatible files and select 'Open in new window'. Enjoy! :)".WriteLineWait();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static void UnInstall()
+        {
+            var regEdit = new RegistryEditor();
+            try
+            {
+                regEdit.RemoveEntries();
+                "Done! ExcelLaunchPad has been uninstalled. You may now delete all ExcelLaunchPad files from your computer.".WriteLineWait();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
