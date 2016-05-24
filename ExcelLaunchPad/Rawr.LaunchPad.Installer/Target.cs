144e7fb08c22046cfe3fcf25eea7cbcfc62adce5
Nick Spreitzer <nick@refactorsaurusrex.com>
2016-05-24 11:13:42 -0500

    Confirm that ELP application exists in correct location

---
+++ ExcelLaunchPad/Rawr.LaunchPad.Installer/Target.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rawr.LaunchPad.Installer
+{
+    public class Target
+    {
+        public string FullPath()
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "ExcelLaunchPad.exe");
+            return File.Exists(path) ? path : null;
+        }
+    }
+}
