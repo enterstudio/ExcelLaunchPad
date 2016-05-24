87cafb476f33a7b1ea2acdad5bfaee2f528c9312
Nick Spreitzer <nick@refactorsaurusrex.com>
2016-05-24 13:20:29 -0500

    Remove registry entries on uninstall + refactor

--- ExcelLaunchPad/Rawr.LaunchPad.Installer/RegistryEditor.cs
+++ ExcelLaunchPad/Rawr.LaunchPad.Installer/RegistryEditor.cs
@@ -9,8 +9,6 @@ namespace Rawr.LaunchPad.Installer
 {
     public class RegistryEditor
     {
-        readonly string targetPath;
-
         readonly List<string> keyNames = new List<string>
         {
             "Excel.CSV",
@@ -24,21 +22,42 @@ namespace Rawr.LaunchPad.Installer
             "Excel.Template.8",
         };
 
-        public RegistryEditor(string targetPath)
+        public void AddEntries(string targetPath)
         {
-            this.targetPath = targetPath;
-        }
-
-        public void AddEntries()
-        {
-            foreach (var name in keyNames)
+            foreach (var name in keyNames.Take(1))
             {
                 using (var registryKey = Registry.ClassesRoot.OpenSubKey(name, true))
+                using (var shell = registryKey?.CreateSubKey("shell"))
+                using (var newWindow = shell?.CreateSubKey("Open in new window"))
+                using (var command = newWindow?.CreateSubKey("command"))
                 {
-                    var shell = registryKey.CreateSubKey("shell");
-                    var newWindow = shell.CreateSubKey("Open in new window");
-                    var command = newWindow.CreateSubKey("command");
+                    if (command == null)
+                        throw new InvalidOperationException($"Unable to set key for '{name}'. Command subkey returned as null. This is likely a permissions issue.");
+
                     command.SetValue(null, $"{targetPath} -f \"%1\"");
+                }
+            }
+        }
+
+        public void RemoveEntries()
+        {
+            foreach (var name in keyNames.Take(1))
+            {
+                using (var registryKey = Registry.ClassesRoot.OpenSubKey(name, true))
+                using (var shell = registryKey?.OpenSubKey("shell", true))
+                {
+                    if (shell == null)
+                        continue;
+
+                    try
+                    {
+                        // Assumption: the key simply doesn't exist
+                        shell.DeleteSubKey("Open in new window");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
                 }
             }
         }
